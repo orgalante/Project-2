631f535922767a199acaae1ebe05feb02435e62b
Or Galante <orgalante@gmail.com>
2023-07-30 11:59:13 +0300

    edited modal

--- coin.ts
+++ coin.ts
@@ -8,15 +8,12 @@ class CoinObj {
             `
     <div class="col-6 col-sm-6 col-md-3 col-lg-2 p-2 ">
         <div class="card text-center rounded-3 " >
-           
             <img src="${this.img}" height="70px" class="card-img-top w-auto mt-3 mx-auto d-block" alt="${this.cId}">
-        
            <div class="card-body p-1 ">
                 <h5 class="card-title">${this.symbol}</h5>
                 <div class = "justify-content-center coin-id-p">
                 <p class=" card-text mb-1 text-center" > ${this.cId}</p>
                 </div>
-                
                 <div class="form-check form-switch ">
                     <input class="form-check-input float-none w-75" type="checkbox" 
                     id="${this.cId}SwitchInput">
@@ -28,7 +25,6 @@ class CoinObj {
                    </span>
                 </button>
                 <div class="collapse" id="${this.cId}Collapse">
-                
                 </div>
             </div>
         </div>
@@ -41,10 +37,10 @@ class CoinObj {
     public getModalCoinHtml() {
         let html =
             `
-        <div class="col-xs-10 col-sm-6 col-md-6 col-lg-4 ">
-            <div class="card text-center ">
-                <img src="${this.img}" class="card-img-top w-50 mx-auto d-block" alt="${this.cId}">
-                <div class="card-body">
+        <div class=" col-6 col-xs-6 col-sm-6 col-md-6 col-lg-4 ">
+            <div class="card text-center rounded-3">
+                <img src="${this.img}"  height="70px" class="card-img-top w-auto mt-3 mx-auto d-block" alt="${this.cId}">
+                <div class="card-body p-1">
                     <h5 class="card-title">${this.symbol}</h5>
                     <p class="card-text">${this.cId}</p>
                     <div class="form-check form-switch ">
